d5f23b20b3070a0e8cc1b528cb03aea4c0b9d1ed
Chaltin Pagan <chaltinpagan@gmail.com>
2017-12-28 13:32:26 -0500

    revise mobile canvas and ternary operators

--- canvas-drawing-v2/canvas-v2.js
+++ canvas-drawing-v2/canvas-v2.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     
-    canvas = document.querySelector("canvas");
+    //canvas id for desktop. canvas2 is for mobile devices and tablets.
+    canvas = document.getElementById("canvas1");
+    canvas2 = document.getElementById("canvas2");
     body = document.querySelector("body");
     body.style.backgroundColor = "#FFFAFA";
     context = canvas.getContext("2d");  
+    context2 = canvas2.getContext("2d");
     box = document.getElementById("box");      
     circle = document.getElementById("circle");    
     triangle = document.getElementById("triangle");    
@@ -17,26 +20,26 @@ document.addEventListener("DOMContentLoaded", (event) => {
     document.addEventListener("keydown", (event) => {
         //Shape coordinates.
         x = Math.floor(Math.random()*900);
-        y = Math.floor(Math.random()*600); //I added 150 to the y-coordinate so shapes won't obstruct the fillText() at the top of the page.
-        let yCoord = () => (y <= 475) ? y : 475; //I wrote this function for the y-coordinate to compensate for the additional 150px added to the original y variable. This way, a shape will not be drawn outside the visible area.
+        y = Math.floor(Math.random()*600);
+        let yCoord = (y <= 475) ? y : 475; //I wrote this ternary operator for the y-coordinate. This way, a shape will not be drawn outside the visible area.
 
-        //For width, height and radius, I wrote conditionals in case a randomly generated dimension is too small to be seen with the naked eye. 
+        //For width, height and radius, I wrote ternary operators in case a randomly generated dimension is too small to be seen with the naked eye. 
         w = Math.floor(Math.random()*100);
-        let width = () => (w < 10 ? 10 : w);
+        let width = (w < 10 ? 10 : w);
         h = Math.floor(Math.random()*100);
-        let height = () => (h < 10 ? 10 : h);
+        let height = (h < 10 ? 10 : h);
         r = Math.floor(Math.random()*100);
-        let circleR = () => (r < 10 ? 10 : r); 
+        let circleR = (r < 10 ? 10 : r); 
 
 
         //I created additional variables for the x-,y-coordinates of the triangles. Without these, the triangles will be concentrated near (0,0).
         xTwo = x + Math.floor(Math.random()*100); //xTwo is the x-coordinate of the second lineTo() of the triangle. 
-        yTwo = yCoord() + Math.floor(Math.random()*100 + 5); //yTwo is the y-coordinate of the second lineTo() of the triangle. 
+        yTwo = yCoord + Math.floor(Math.random()*100 + 5); //yTwo is the y-coordinate of the second lineTo() of the triangle. 
         xThree = x + Math.floor(Math.random()*100); //xThree is the x-coordinate of the thrid lineTo() of the triangle.
-        yThree = yCoord() + Math.floor(Math.random()*100 + 5); //yThree is the y-coordinate of the third lineTo() of the triangle.
+        yThree = yCoord + Math.floor(Math.random()*100 + 5); //yThree is the y-coordinate of the third lineTo() of the triangle.
 
         //Capital letters draw lines. This will calculate the length of the line.
-        sqrt = Math.floor(Math.sqrt(Math.pow(xTwo - x, 2) + Math.pow(yTwo - y, 2)) ); 
+        length = Math.floor(Math.sqrt(Math.pow(xTwo - x, 2) + Math.pow(yTwo - y, 2)) ); 
 
         //I originally used RGB values for my shapes. For some reason, Chrome displayed all the random colors as black. I changed the random color generator to hex values.
         // r = Math.random()*255
@@ -45,157 +48,135 @@ document.addEventListener("DOMContentLoaded", (event) => {
         //context.strokeStyle = "rgb("+r+","+g+","+b+")";
 
         //The following random hex generator code comes from Paul Irish at https://www.paulirish.com/2009/random-hex-color-code-snippets/
-        //The below hex color generator sometimes generates an invalid hex code (five characters after the hashtag instead of six). I wrote a conditional to address that outcome. If an invalid hex code is generated, then the shape will be a fallback color. The fallback color is a light gray.
+        //The below hex color generator sometimes generates an invalid hex code (five characters after the hashtag instead of six). I wrote a ternary operator to address that outcome. If an invalid hex code is generated, then the shape will be a fallback color. The fallback color is a light gray.
         hex = '#'+ Math.floor(Math.random()*16777215).toString(16);
-        let colorTwo = () => (hex.length === 7) ? hex : "#d3d3d3";
+        let colorTwo = (hex.length === 7) ? hex : "#d3d3d3";
         
 
         //Start if else statement for keydown event.
         if (event.key.match(/[a-z]/) && event.key.length === 1) {
             //All lowercase letter keys will produce a quadrilateral. I added a length boolean because I wanted to limit quadrilaterals to single-letter keypress events. Keys such as "Enter" and "Backspace" will produce a triangle.
-            context.strokeStyle = colorTwo();
-            context.strokeRect(x, yCoord(), width(), height());
-            console.log(`Lowercase ${event.key} draws a rectangle.\nColor is ${colorTwo()}.\nCoordinates are ${x}, ${yCoord()}.\nHeight is ${height()} and width is ${width()}.`);
+            context.strokeStyle = colorTwo;
+            context.strokeRect(x, yCoord, width, height);
+            console.log(`Lowercase ${event.key} draws a rectangle.\nColor is ${colorTwo}.\nCoordinates are ${x}, ${yCoord}.\nHeight is ${height} and width is ${width}.`);
         } else if (event.shiftKey && event.key.match(/[A-Z]/) && event.key.length === 1) {
             //Capital letters will produce a line.
             //Since I'm using keydown instead of keypress event listener, I now have to test for a Shift key event.
             //Using keydown instead of keypress event listener means a triangle will be produced in addition to a line. This happens because keys that don't produce a character value will draw a triangle. See the else statement.
-            context.strokeStyle = colorTwo();
+            context.strokeStyle = colorTwo;
             context.beginPath();
-            context.moveTo(x, yCoord());
+            context.moveTo(x, yCoord);
             context.lineTo(xTwo, yTwo); 
             context.stroke(); 
-            console.log(`Uppercase ${event.key} draws a line.\nColor is ${colorTwo()}.\nStarting coordinates are ${x}, ${yCoord()}.\nEnding coordinates are ${xTwo}, ${yTwo}.\nLine length is ${sqrt}.`);
+            console.log(`Uppercase ${event.key} draws a line.\nColor is ${colorTwo}.\nStarting coordinates are ${x}, ${yCoord}.\nEnding coordinates are ${xTwo}, ${yTwo}.\nLine length is ${length}.`);
         } else if (event.key.match(/[0-9]/)){
             //All number keys (0-9) will produce a circle.
-            context.strokeStyle = colorTwo();
+            context.strokeStyle = colorTwo;
             context.beginPath();
-            context.arc(x,yCoord(),circleR(),0,2*Math.PI);
+            context.arc(x,yCoord,circleR,0,2*Math.PI);
             context.stroke();
-            console.log(`Number ${event.key} draws a circle.\nColor is ${colorTwo()}.\nCoordinates are ${x}, ${yCoord()}.\nRadius is ${circleR()}.`);
+            console.log(`Number ${event.key} draws a circle.\nColor is ${colorTwo}.\nCoordinates are ${x}, ${yCoord}.\nRadius is ${circleR}.`);
         } else {
             // All other keys will produce a triangle. Includes Shift, Ctrl, Alt, and arrow keys.
-            context.strokeStyle = colorTwo();
+            context.strokeStyle = colorTwo;
             context.beginPath();
-            context.moveTo(x, yCoord());
+            context.moveTo(x, yCoord);
             context.lineTo(xTwo, yTwo); 
             context.lineTo(xThree,yThree); 
             context.closePath(); 
             context.stroke(); 
-            console.log(`The ${event.key} key draws a triangle.\nColor is ${colorTwo()}.\nStarting point coordinates are ${x}, ${yCoord()}.\nSecond point coordinates are ${xTwo}, ${yTwo}.\nThird point coordinates are ${xThree}, ${yThree}.`);
+            console.log(`The ${event.key} key draws a triangle.\nColor is ${colorTwo}.\nStarting point coordinates are ${x}, ${yCoord}.\nSecond point coordinates are ${xTwo}, ${yTwo}.\nThird point coordinates are ${xThree}, ${yThree}.`);
         }
     });   
 
     
-    //This event listener is for mobile devices.
+    //This event listener is for mobile devices and tablets.
     box.addEventListener("touchend", (event) => {
-        let e = event.target.id;
-        console.log(e);
-        console.log(event);
         //Shape coordinates.
-        x = Math.floor(Math.random()*900);
-        y = Math.floor(Math.random()*600); //I added 150 to the y-coordinate so shapes won't obstruct the fillText() at the top of the page.
-        let yCoord = () => (y <= 475) ? y : 475; //I wrote this function for the y-coordinate to compensate for the additional 150px added to the original y variable. This way, a shape will not be drawn outside the visible area.
+        x = Math.floor(Math.random()*500);
+        y = Math.floor(Math.random()*300);
+        let yCoord = (y <= 195) ? y : 195; //I wrote this ternary operator for the y-coordinate. This way, a shape will not be drawn outside the visible area.
 
-        //For width, height and radius, I wrote conditionals in case a randomly generated dimension is too small to be seen with the naked eye. 
+        //For width, height and radius, I wrote ternary operators in case a randomly generated dimension is too small to be seen with the naked eye. 
         w = Math.floor(Math.random()*100);
-        let width = () => (w < 10 ? 10 : w);
+        let width = (w < 20 ? 20 : w);
         h = Math.floor(Math.random()*100);
-        let height = () => (h < 10 ? 10 : h);
-        
+        let height = (h < 20 ? 20 : h);
+    
         hex = '#'+ Math.floor(Math.random()*16777215).toString(16);
-        let colorTwo = () => (hex.length === 7) ? hex : "#d3d3d3";
+        let colorTwo = (hex.length === 7) ? hex : "#d3d3d3";
 
-        //All lowercase letter keys will produce a quadrilateral. I added a length boolean because I wanted to limit quadrilaterals to single-letter keypress events. Keys such as "Enter" and "Backspace" will produce a triangle.
-        context.strokeStyle = colorTwo();
-        context.strokeRect(x, yCoord(), width(), height());
-        console.log(`Lowercase ${event.key} draws a rectangle.\nColor is ${colorTwo()}.\nCoordinates are ${x}, ${yCoord()}.\nHeight is ${height()} and width is ${width()}.`);
+        context2.strokeStyle = colorTwo;
+        context2.strokeRect(x, yCoord, width, height);
+        console.log(`Lowercase ${event.key} draws a rectangle.\nColor is ${colorTwo}.\nCoordinates are ${x}, ${yCoord}.\nHeight is ${height} and width is ${width}.`);
     });
 
-    //This event listener is for mobile devices.
+    //This event listener is for mobile devices and tablets.
     line.addEventListener("touchend", (event) => {
-        let e = event.target.id;
-        console.log(e);
-        console.log(event);
-        //Shape coordinates.
-        x = Math.floor(Math.random()*900);
-        y = Math.floor(Math.random()*600); //I added 150 to the y-coordinate so shapes won't obstruct the fillText() at the top of the page.
-        let yCoord = () => (y <= 475) ? y : 475; //I wrote this function for the y-coordinate to compensate for the additional 150px added to the original y variable. This way, a shape will not be drawn outside the visible area.
+        x = Math.floor(Math.random()*500);
+        y = Math.floor(Math.random()*300);
+        let yCoord = (y <= 195) ? y : 195; //I wrote this ternary operator for the y-coordinate. This way, a shape will not be drawn outside the visible area.
 
-        //For the circles, I created additional variables for the x-,y-coordinates of the triangles. Without these, the triangles will be concentrated near (0,0).
-        xTwo = x + Math.floor(Math.random()*100); //xTwo is the x-coordinate of the second lineTo() of the triangle. 
-        yTwo = yCoord() + Math.floor(Math.random()*100 + 5); //yTwo is the y-coordinate of the second lineTo() of the triangle. 
+        xTwo = x + Math.floor(Math.random()*50); //xTwo is the x-coordinate of the second lineTo() of the triangle. 
+        yTwo = yCoord + Math.floor(Math.random()*50 + 5); //yTwo is the y-coordinate of the second lineTo() of the triangle. 
 
-        //Capital letters draw lines. This will calculate the length of the line.
-        sqrt = Math.floor(Math.sqrt(Math.pow(xTwo - x, 2) + Math.pow(yTwo - y, 2)) ); 
+        length = Math.floor(Math.sqrt(Math.pow(xTwo - x, 2) + Math.pow(yTwo - y, 2)) ); 
     
         //Random color generator.
         hex = '#'+ Math.floor(Math.random()*16777215).toString(16);
-        let colorTwo = () => (hex.length === 7) ? hex : "#d3d3d3";
+        let colorTwo = (hex.length === 7) ? hex : "#d3d3d3";
 
-        //Capital letters will produce a line.
-        //Since I'm using keydown instead of keypress event listener, I now have to test for a Shift key event.
-        //Using keydown instead of keypress event listener means a triangle will be produced in addition to a line. This happens because keys that don't produce a character value will draw a triangle. See the else statement.
-        context.strokeStyle = colorTwo();
-        context.beginPath();
-        context.moveTo(x, yCoord());
-        context.lineTo(xTwo, yTwo); 
-        context.stroke(); 
-        console.log(`Uppercase ${event.key} draws a line.\nColor is ${colorTwo()}.\nStarting coordinates are ${x}, ${yCoord()}.\nEnding coordinates are ${xTwo}, ${yTwo}.\nLine length is ${sqrt}.`);
+        context2.strokeStyle = colorTwo;
+        context2.beginPath();
+        context2.moveTo(x, yCoord);
+        context2.lineTo(xTwo, yTwo); 
+        context2.stroke(); 
+        console.log(`Uppercase ${event.key} draws a line.\nColor is ${colorTwo}.\nStarting coordinates are ${x}, ${yCoord}.\nEnding coordinates are ${xTwo}, ${yTwo}.\nLine length is ${length}.`);
     });
         
-    //This event listener is for mobile devices.
+    //This event listener is for mobile devices and tablets.
     circle.addEventListener("touchend", (event) => {
-        let e = event.target.id;
-        console.log(e);
-        console.log(event);
         //Shape coordinates.
-        x = Math.floor(Math.random()*900);
-        y = Math.floor(Math.random()*600); //I added 150 to the y-coordinate so shapes won't obstruct the fillText() at the top of the page.
-        let yCoord = () => (y <= 475) ? y : 475; //I wrote this function for the y-coordinate to compensate for the additional 150px added to the original y variable. This way, a shape will not be drawn outside the visible area.
+        x = Math.floor(Math.random()*500);
+        y = Math.floor(Math.random()*300);
+        let yCoord = (y <= 245) ? y : 245; //I wrote this ternary operator for the y-coordinate. This way, a shape will not be drawn outside the visible area.
 
         //For width, height and radius, I wrote conditionals in case a randomly generated dimension is too small to be seen with the naked eye. 
-        r = Math.floor(Math.random()*100);
-        let circleR = () => (r < 10 ? 10 : r); 
+        r = Math.floor(Math.random()*50);
+        let circleR = (r < 10 ? 10 : r); 
     
         hex = '#'+ Math.floor(Math.random()*16777215).toString(16);
-        let colorTwo = () => (hex.length === 7) ? hex : "#d3d3d3";
+        let colorTwo = (hex.length === 7) ? hex : "#d3d3d3";
 
-        //All number keys (0-9) will produce a circle.
-        context.strokeStyle = colorTwo();
-        context.beginPath();
-        context.arc(x,yCoord(),circleR(),0,2*Math.PI);
-        context.stroke();
-        console.log(`Number ${event.key} draws a circle.\nColor is ${colorTwo()}.\nCoordinates are ${x}, ${yCoord()}.\nRadius is ${circleR()}.`);
+        context2.strokeStyle = colorTwo;
+        context2.beginPath();
+        context2.arc(x,yCoord,circleR,0,2*Math.PI);
+        context2.stroke();
+        console.log(`Number ${event.key} draws a circle.\nColor is ${colorTwo}.\nCoordinates are ${x}, ${yCoord}.\nRadius is ${circleR}.`);
     });
 
-    //This event listener is for mobile devices.
+    //This event listener is for mobile devices and tablets.
     triangle.addEventListener("touchend", (event) => {
-        let e = event.target.id;
-        console.log(e);
-        console.log(event);
         //Shape coordinates.
-        x = Math.floor(Math.random()*900);
-        y = Math.floor(Math.random()*600); //I added 150 to the y-coordinate so shapes won't obstruct the fillText() at the top of the page.
-        let yCoord = () => (y <= 475) ? y : 475; //I wrote this function for the y-coordinate to compensate for the additional 150px added to the original y variable. This way, a shape will not be drawn outside the visible area.
+        x = Math.floor(Math.random()*500);
+        y = Math.floor(Math.random()*300);
+        let yCoord = (y <= 195) ? y : 195; //I wrote this ternary operator for the y-coordinate. This way, a shape will not be drawn outside the visible area.
 
-        //For the circles, I created additional variables for the x-,y-coordinates of the triangles. Without these, the triangles will be concentrated near (0,0).
-        xTwo = x + Math.floor(Math.random()*100); //xTwo is the x-coordinate of the second lineTo() of the triangle. 
-        yTwo = yCoord() + Math.floor(Math.random()*100 + 5); //yTwo is the y-coordinate of the second lineTo() of the triangle. 
+        xTwo = x + Math.floor(Math.random()*50); //xTwo is the x-coordinate of the second lineTo() of the triangle. 
+        yTwo = yCoord + Math.floor(Math.random()*50 + 5); //yTwo is the y-coordinate of the second lineTo() of the triangle. 
         xThree = x + Math.floor(Math.random()*100); //xThree is the x-coordinate of the thrid lineTo() of the triangle.
-        yThree = yCoord() + Math.floor(Math.random()*100 + 5); //yThree is the y-coordinate of the third lineTo() of the triangle. 
+        yThree = yCoord + Math.floor(Math.random()*50 + 5); //yThree is the y-coordinate of the third lineTo() of the triangle. 
 
         hex = '#'+ Math.floor(Math.random()*16777215).toString(16);
-        let colorTwo = () => (hex.length === 7) ? hex : "#d3d3d3";
+        let colorTwo = (hex.length === 7) ? hex : "#d3d3d3";
 
-        // All other keys will produce a triangle. Includes Shift, Ctrl, Alt, and arrow keys.
-        context.strokeStyle = colorTwo();
-        context.beginPath();
-        context.moveTo(x, yCoord());
-        context.lineTo(xTwo, yTwo); 
-        context.lineTo(xThree,yThree); 
-        context.closePath(); 
-        context.stroke(); 
-        console.log(`The ${event.key} key draws a triangle.\nColor is ${colorTwo()}.\nStarting point coordinates are ${x}, ${yCoord()}.\nSecond point coordinates are ${xTwo}, ${yTwo}.\nThird point coordinates are ${xThree}, ${yThree}.`);
+        context2.strokeStyle = colorTwo;
+        context2.beginPath();
+        context2.moveTo(x, yCoord);
+        context2.lineTo(xTwo, yTwo); 
+        context2.lineTo(xThree,yThree); 
+        context2.closePath(); 
+        context2.stroke(); 
+        console.log(`The ${event.key} key draws a triangle.\nColor is ${colorTwo}.\nStarting point coordinates are ${x}, ${yCoord}.\nSecond point coordinates are ${xTwo}, ${yTwo}.\nThird point coordinates are ${xThree}, ${yThree}.`);
     });
 });
