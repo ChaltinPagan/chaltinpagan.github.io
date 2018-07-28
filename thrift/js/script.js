cee61b764fac9caa79503c3a127e762f3b036786
Chaltin Pagan <chaltinpagan@Chaltin-Pagan.fios-router.home>
2018-07-27 22:21:05 -0400

    Adding jQuery to Stores page

---
+++ thrift/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    // Animation for Stores page
+
+    // Goodwill section will be displayed by default. Hide other sections.
+    $('#goodwill').siblings('section').hide();
+
+    // Goodwill button
+    $('#goodwill-button').click(function() {
+        // $('#housing-works').hide();
+        // $('#salvation-army').hide();
+        $('#goodwill').animate({
+            height: 'toggle'
+        }, 'slow');
+        $('#goodwill').siblings('section').animate({
+            height: 'hide'
+        }, 'slow')
+    });
+
+    // Housing Works button
+    $('#housing-works-button').click(function() {
+        $('#housing-works').animate({
+            height: 'toggle'
+        }, 'slow');
+        $('#housing-works').siblings('section').animate({
+            height: 'hide'
+        }, 'slow');
+    });
+
+    // Salvation Army button
+    $('#salvation-army-button').click(function() {
+        $('#salvation-army').animate({
+            height: 'toggle'
+        }, 'slow');
+        $('#salvation-army').siblings('section').animate({
+            height: 'hide'
+        }, 'slow');
+    });
+});
